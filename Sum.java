91cb2dc7243b76bc3e102e50fa39fc410595f4c7
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-18 21:12:06 +0530

    Update and rename README.md to Sum.java

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+  public class Sum{
+    public static void main(String args[]){
+      Scanner sc=new scanner(System.in);
+      System.out.println("Enter Any Two Integer value:");
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      System.out.println("Sum:"+(a+b));
+     }
+   }  
